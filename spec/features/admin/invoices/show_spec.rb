97f886ca5618583cae2ba750990c35bf47bbb0fc
Matt Kragen <mattdkragen@gmail.com>
2021-07-27 21:36:52 -0700

    Begin testing invoice item information

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -14,4 +14,23 @@ RSpec.describe 'Admin Invoice show page' do
     expect(page).to have_content(@invoice_1.customer.first_name)
     expect(page).to have_content(@invoice_1.customer.last_name)
   end
+
+  it 'shows invoice items and item information' do
+    visit admin_invoice_path(@invoice_1)
+
+    # Price
+    expect(page).to have_content()
+    # Quantity
+    expect(page).to have_content()
+    # Price
+    expect(page).to have_content()
+    # Status
+    expect(page).to have_content()
+  end
+  # When I visit an admin invoice show page
+  # Then I see all of the items on the invoice including:
+  # - Item name
+  # - The quantity of the item ordered
+  # - The price the Item sold for
+  # - The Invoice Item status
 end
