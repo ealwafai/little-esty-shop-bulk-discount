6d768bf6b84b737f35a6ceb78b38f366c98f9406
Matt Kragen <mattdkragen@gmail.com>
2021-07-27 20:24:21 -0700

    Add link to admin invoice show page

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe 'Admin Invoices index page' do
   it 'links each id to invoice show page' do
     visit admin_invoices_path
     click_link "#{@invoice_1.id}"
-    expect(current_path).to eq(admin_invoice_path(invoice_1))
+    expect(current_path).to eq(admin_invoice_path(@invoice_1))
   end
-  # Each id links to the admin invoice show page
 end
