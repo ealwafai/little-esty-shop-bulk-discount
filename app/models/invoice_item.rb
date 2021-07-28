6c27401554d710347bdb87b5de3ecc2d0549884d
Matt Kragen <mattdkragen@gmail.com>
2021-07-28 09:21:52 -0700

    Test and create method for dollar amt display

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,4 +2,8 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
   enum status: [ :pending, :packaged, :shipped ]
+
+  def price_display
+    unit_price / 100.00
+  end
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -16,8 +16,19 @@ RSpec.describe InvoiceItem, type: :model do
   #   describe '.' do
   #   end
   # end
-  # describe 'instance methods' do
-  #   describe '#' do
-  #   end
-  # end
+  describe 'instance methods' do
+    describe '#price_display' do
+      it 'displays unit_price in dollar amount' do
+        invoice = create(:invoice)
+        item = create(:item)
+        invoice_item = InvoiceItem.create!(
+          invoice: invoice,
+          item: item,
+          quantity: 1,
+          unit_price: 11111
+        )
+        expect(invoice_item.price_display).to eq(111.11)
+      end
+    end
+  end
 end
