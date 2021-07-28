e9fd12f81b7dffa5db2b00333b6d134b87b6019a
Matt Kragen <mattdkragen@gmail.com>
2021-07-27 22:01:29 -0700

    Develop tests further

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Admin Invoice show page' do
   before :each do
     @invoice_1 = create(:invoice)
+    @item_1 = create(:item)
+    @invoice_item_1 = InvoiceItem.create!(
+      invoice: @invoice_1, item: @item_1, quantity: 1, status: 0
+    )
   end
 
   it 'shows information regarding the invoice' do
@@ -18,14 +22,14 @@ RSpec.describe 'Admin Invoice show page' do
   it 'shows invoice items and item information' do
     visit admin_invoice_path(@invoice_1)
 
-    # Price
-    expect(page).to have_content()
+    # name
+    expect(page).to have_content(@item_1.name)
     # Quantity
-    expect(page).to have_content()
+    expect(page).to have_content(@item_1.quantity)
     # Price
-    expect(page).to have_content()
+    expect(page).to have_content(@item_1.unit_price)
     # Status
-    expect(page).to have_content()
+    expect(page).to have_content(@item_1.status)
   end
   # When I visit an admin invoice show page
   # Then I see all of the items on the invoice including:
