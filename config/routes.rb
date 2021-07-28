47314fbf77628b795a0e678bd925c2df17d5e912
Matt Kragen <mattdkragen@gmail.com>
2021-07-28 16:32:42 -0700

    Create starter for admin dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
-    resources only: [:index]
+    # resources only: [:index]
     resources :merchants
   end
+
+  resources :admin, controller: 'admin/dashboard', only: [:index]
+
 end
