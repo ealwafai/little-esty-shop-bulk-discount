64019308ba474ec9dade6eb0982468ec5d71e151
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-29 13:20:35 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :merchants do
+    resources :invoices, only: [:index, :show]
+  end
 end
