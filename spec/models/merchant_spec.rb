ec0f128e04a61a4ee5a5e51348d1b31edfedef00
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-29 13:22:31 -0600

    Delete extra info

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,18 +4,4 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many(:items) }
   end
-  # describe 'validations' do
-  #   it { should validate_presence_of(:) }
-  # end
-  # before :each do
-  #
-  # end
-  # describe 'class methods' do
-  #   describe '.' do
-  #   end
-  # end
-  # describe 'instance methods' do
-  #   describe '#' do
-  #   end
-  # end
 end
