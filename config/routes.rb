7942e6feb0dd0edff24998d2bdc3fa7bccb05b10
Matt Kragen <mattdkragen@gmail.com>
2021-07-29 10:56:29 -0700

    Resolve merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
-    # resources only: [:index]
     resources :merchants
     resources :invoices
+    resources :dashboard, only: [:index]
   end
 
   resources :merchants do
     resources :items
   end
-
-  resources :admin, controller: 'admin/dashboard', only: [:index]
 
 end
