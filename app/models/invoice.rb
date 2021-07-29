156b4967f39140f6fd822675c64ceb6cb6c34de9
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-29 13:24:30 -0600

    Create a merchants invoices class method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,8 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
   has_many :transactions
   enum status: [ 'in progress', :completed, :cancelled ]
+
+  def self.merchants_invoices(merch_id)
+    joins(:items).where('merchant_id = ?', merch_id).select("invoices.*").distinct
+  end
 end
