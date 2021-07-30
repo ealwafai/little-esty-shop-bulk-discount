07f1e93bb682535a90526c90ad029a28efaec86e
Matt Kragen <mattdkragen@gmail.com>
2021-07-30 13:25:08 -0700

    Write #top_five method for Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,13 @@
 class Customer < ApplicationRecord
   has_many :invoices, :dependent => :destroy
+  has_many :transactions, through: :invoices
+
+  def self.top_five
+    joins(:transactions)
+      .where("result = ?", "success")
+      .group(:id)
+      .select("customers.*, count('transactions.result') as top")
+      .order(top: :desc)
+      .limit(5)
+  end
 end
