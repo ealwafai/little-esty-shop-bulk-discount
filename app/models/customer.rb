2565a1f4659df47c951c062db052f607d2cb9aac
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-31 11:35:08 -0600

    Create top five customer class method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
   has_many :invoices, :dependent => :destroy
+
+  def self.top_five_customers_by_transactions(merchant_id)
+    joins(invoices: [:transactions, :items])
+    .where('result = ? and merchant_id = ?', "success", merchant_id)
+    .select("customers.*, count('transactions.result') AS successful_transactions")
+    .group("customers.id")
+    .order(successful_transactions: :desc)
+    .limit(5)
+  end
 end
