649e7e87bad8afbc08f4f458b6d1bd8c0e4a6a9f
Matt Kragen <mattdkragen@gmail.com>
2021-07-30 17:01:06 -0700

    Write tests for incomplete invoice display order

--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -62,9 +62,6 @@ RSpec.describe 'Admin Deshboard/Index page' do
   end
 
   it 'displays incomplete invoices' do
-    inv_id_1 = "ID: #{@invoice_4.id}"
-    inv_id_2 = "ID: #{@invoice_5.id}"
-
     visit admin_index_path
     expect(page).to have_content('Incomplete Invoices')
 
@@ -72,19 +69,31 @@ RSpec.describe 'Admin Deshboard/Index page' do
       expect(page).to_not have_content(@invoice_1.id)
       expect(page).to have_content(@invoice_4.id)
       expect(page).to have_content(@invoice_5.id)
-      expect(inv_id_1).to appear_before(inv_id_2)
     end
   end
   
-  xit 'links to invoice show pages through their IDs' do
+  it 'links to invoice show pages through their IDs' do
     visit admin_index_path
-  end
-  # And each invoice id links to that invoice's admin show page
 
-  xit 'displays creation dates of invoices ordered oldest to newest' do
-    visit admin_index_path
+    within("#incomplete_invoices") do
+      click_link "#{@invoice_4.id}"
+    end
+
+    expect(current_path).to eq(admin_invoice_path(@invoice_4))
   end
-  # When I visit the admin dashboard
+
+  it 'displays creation dates of invoices ordered oldest to newest' do
+    inv_id_1 = "ID: #{@invoice_4.id}"
+    inv_id_2 = "ID: #{@invoice_5.id}"
+
+    visit admin_index_path
+
+    within("#incomplete_invoices") do
+      expect(page).to have_content("Date: #{@invoice_4.created_at_display}")
+      expect(page).to have_content("Date: #{@invoice_5.created_at_display}")
+      expect(inv_id_1).to appear_before(inv_id_2)
+    end
+  end
   # In the section for "Incomplete Invoices",
   # Next to each invoice id I see the date that the invoice was created
   # And I see the date formatted like "Monday, July 18, 2019"
