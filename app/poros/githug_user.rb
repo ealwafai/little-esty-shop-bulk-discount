8e2921f68c6a98a9e21eb86876c3c8cf0cd191e7
Amanda McGuire <aruwart001@gmail.com>
2021-08-02 12:07:53 -0600

    Add new classes for api searches

--- app/poros/githug_user.rb
+++
@@ -1,2 +0,0 @@
-class GithubUser
-end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,7 +1,16 @@
 class GithubService
-  def self.user_info(username)
-    response = Faraday.get "https://api.github.com/users/#{username}"
-    body = response.body
-    JSON.parse(body, symbolize_names: true)
+  def self.repo_info
+    response = Faraday.get("https://api.github.com/repos/InOmn1aParatus/little-esty-shop")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.contributor_info
+    response = Faraday.get("https://api.github.com/repos/InOmn1aParatus/little-esty-shop/contributors")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.pulls_info
+    response = Faraday.get("https://api.github.com/repos/InOmn1aParatus/little-esty-shop/pulls?state=closed")
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
