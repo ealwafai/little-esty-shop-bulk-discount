bac9087bfde63c424e00f1ddb62db65ab11f1020
Carina Sweet <carina.sweet06@gmail.com>
2021-08-02 17:39:34 -0600

    Move status metods to application record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  def self.status_enabled
+    where(status: 'enabled')
+  end
+
+  def self.status_disabled
+    where(status: 'disabled')
+  end
+
   def short_date
     strftime('%m/%d/%y')
   end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,14 +1,6 @@
 class Merchant < ApplicationRecord
   has_many :items, :dependent => :destroy
 
-  def self.enabled
-    where(status: 'enabled')
-  end
-
-  def self.disabled
-    where(status: 'disabled')
-  end
-
   def self.top_five_by_revenue
     joins(items: [invoices: :transactions]).select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price/100.0) as revenue').where("transactions.result = 'success'").group('merchants.id').order('revenue DESC').limit(5)
   end
