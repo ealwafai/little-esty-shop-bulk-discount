126ead3ca50d230642eb767ebf69788cfa7ec065
Carina Sweet <carina.sweet06@gmail.com>
2021-08-02 17:40:04 -0600

    Remove transactions assosiation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,18 +1,19 @@
 class Customer < ApplicationRecord
   has_many :invoices, :dependent => :destroy
-  has_many :transactions, through: :invoices
 
   def self.top_five
-    joins(:transactions)
-      .where("result = ?", "success")
-      .group(:id)
-      .select("customers.*, count('transactions.result') as top")
-      .order(top: :desc)
-      .limit(5)
+    joins(invoices: :transactions)
+    .where("transactions.result = ?", "success")
+    .group(:id)
+    .select("customers.*, count('transactions.result') as top")
+    .order(top: :desc)
+    .limit(5)
   end
 
   def successful_transactions
-    transactions.where('result = ?', 'success').count
+    invoices.joins(:transactions)
+            .where('transactions.result = ?', 'success')
+            .count
   end
 
   def self.top_five_customers_by_transactions(merchant_id)
