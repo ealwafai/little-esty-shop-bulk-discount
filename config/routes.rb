deab805c63665032502af9745b36daa2d7952ced
Carina Sweet <carina.sweet06@gmail.com>
2021-08-02 17:43:02 -0600

    Remove duplicate route

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,4 @@ Rails.application.routes.draw do
     resources :items
     resources :invoices, only: [:index, :show]
   end
-
-  resources :admin, controller: 'admin/dashboard', only: [:index]
-
 end
