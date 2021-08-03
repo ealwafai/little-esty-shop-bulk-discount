2d451d11ea2c12ec806fa5a69007935c3de22faf
Amanda McGuire <aruwart001@gmail.com>
2021-08-02 20:51:48 -0600

    Add links to welcome page and change link text to black

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe 'Welcome Index Page' do
   it 'shows the name of the project' do
     expect(page).to have_content('Little Esty Shop')
   end
+
+  it 'can link to admin dashboard and merchants index' do
+    expect(page).to have_link('Admin Dashboard')
+    expect(page).to have_link('Merchants Index')
+
+    click_link 'Admin Dashboard'
+    expect(current_path).to eq(admin_index_path)
+
+    visit '/'
+    click_link 'Merchants Index'
+    expect(current_path).to eq(merchants_path)
+  end
 end
