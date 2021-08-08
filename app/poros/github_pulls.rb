b9ce323088bc876c45e106d816dc7f8e5a6317a4
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-08-07 21:46:20 -0600

    Comment out API stuff that was causing errors

--- app/poros/github_pulls.rb
+++ app/poros/github_pulls.rb
@@ -5,11 +5,11 @@ class GithubPulls
     @pull_requests = add_pulls(data)
   end
 
-  def add_pulls(data)
-    hash = Hash.new(0)
-    data.each do |pull|
-      hash[pull[:user][:login]] += 1
-    end
-    hash
-  end
+  # def add_pulls(data)
+  #   hash = Hash.new(0)
+  #   data.each do |pull|
+  #     hash[pull[:user][:login]] += 1
+  #   end
+  #   hash
+  # end
 end
