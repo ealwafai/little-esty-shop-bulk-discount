c5fc7322c5b01f558426d8bb8048bd631e65314d
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-08-07 22:50:08 -0600

    Add before each and edit discount test

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Discount show page' do
+  before :each do
+    @merchant = create(:merchant)
+    @bulk_discount = BulkDiscount.create!(name: 'BD 1', percentage: 10, threshold: 20, merchant: @merchant)
+
+    visit merchant_bulk_discount_path(@merchant, @bulk_discount)
+  end
+
   it 'displays the discount threshold for number of items to be purchased and the percentage off' do
-    merchant = create(:merchant)
-    bulk_discount = BulkDiscount.create!(name: 'BD 1', percentage: 10, threshold: 20, merchant: merchant)
+    expect(page).to have_content(@bulk_discount.name)
+    expect(page).to have_content("Percentage Discounted: #{@bulk_discount.percentage}%")
+    expect(page).to have_content("Number of Items for Eligibility: #{@bulk_discount.threshold}+")
+  end
 
-    visit merchant_bulk_discount_path(merchant, bulk_discount)
+  it 'displays a link to edit an existing discount' do
+    expect(page).to have_link('Edit')
 
-    expect(page).to have_content(bulk_discount.name)
-    expect(page).to have_content("Percentage Discounted: #{bulk_discount.percentage}%")
-    expect(page).to have_content("Number of Items for Eligibility: #{bulk_discount.threshold}+")
+    click_on('Edit')
+
+    expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant, @bulk_discount))
   end
 end
