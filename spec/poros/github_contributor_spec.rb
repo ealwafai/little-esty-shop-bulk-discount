f4b272eba0976f5fa95da2f0dc135492e6a5157d
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-08-07 22:48:57 -0600

    Comment out group API

--- spec/poros/github_contributor_spec.rb
+++ spec/poros/github_contributor_spec.rb
@@ -1,24 +1,24 @@
-require 'rails_helper'
-
-RSpec.describe GithubContributor do
-  before :each do
-    @contributor_info = [
-      {id: 56685055,:login=>"InOmn1aParatus", :contributions=>75},
-      {id: 79383963,:login=>"amcguire17", :contributions=>42},
-      {id: 77654906,:login=>"ealwafai", :contributions=>41},
-      {id: 81600649,:login=>"chsweet", :contributions=>35},
-      {id: 8150461,:login=>"BrianZanti", :contributions=>33}
-    ]
-  end
-
-  it 'returns a contributors attribute' do
-    repo = GithubContributor.new(@contributor_info)
-    expected = {
-      "InOmn1aParatus" => 75,
-      "amcguire17" => 42,
-      "ealwafai" => 41,
-      "chsweet" => 35
-    }
-    expect(repo.contributors).to eq(expected)
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe GithubContributor do
+#   before :each do
+#     @contributor_info = [
+#       {id: 56685055,:login=>"InOmn1aParatus", :contributions=>75},
+#       {id: 79383963,:login=>"amcguire17", :contributions=>42},
+#       {id: 77654906,:login=>"ealwafai", :contributions=>41},
+#       {id: 81600649,:login=>"chsweet", :contributions=>35},
+#       {id: 8150461,:login=>"BrianZanti", :contributions=>33}
+#     ]
+#   end
+#
+#   it 'returns a contributors attribute' do
+#     repo = GithubContributor.new(@contributor_info)
+#     expected = {
+#       "InOmn1aParatus" => 75,
+#       "amcguire17" => 42,
+#       "ealwafai" => 41,
+#       "chsweet" => 35
+#     }
+#     expect(repo.contributors).to eq(expected)
+#   end
+# end
--- spec/poros/github_repo_spec.rb
+++ spec/poros/github_repo_spec.rb
@@ -1,12 +1,12 @@
-require 'rails_helper'
-
-RSpec.describe GithubRepo do
-  before :each do
-    @repo_info = {:id=>389774449, :name=>"little-esty-shop"}
-  end
-
-  it 'returns a name attribute' do
-    repo = GithubRepo.new(@repo_info)
-    expect(repo.name).to eq("little-esty-shop")
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe GithubRepo do
+#   before :each do
+#     @repo_info = {:id=>389774449, :name=>"little-esty-shop"}
+#   end
+#
+#   it 'returns a name attribute' do
+#     repo = GithubRepo.new(@repo_info)
+#     expect(repo.name).to eq("little-esty-shop")
+#   end
+# end
