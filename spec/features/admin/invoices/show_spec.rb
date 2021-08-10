e45947eb597a3f27f4f073ba7eb67de428dae38a
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-08-10 09:46:16 -0600

    Create test for admin invoice total/discounted revenue

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -2,52 +2,57 @@ require 'rails_helper'
 
 RSpec.describe 'Admin Invoice show page' do
   before :each do
-    @invoice_1 = create(:invoice, status: 'in progress')
-    @item_1 = create(:item)
-    @invoice_item_1 = InvoiceItem.create!(
-      invoice: @invoice_1,
-      item: @item_1,
-      quantity: 1,
-      status: 0,
-      unit_price: 11111
-    )
+    @merchant = create(:merchant)
+
+    @customer = create(:customer)
+
+    @invoice = create(:invoice, status: 'in progress', customer_id: @customer.id)
+
+    @item_1 = create(:item, merchant_id: @merchant.id)
+    @item_2 = create(:item, merchant_id: @merchant.id)
+    @item_3 = create(:item, merchant_id: @merchant.id)
+
+    @invoice_item_1 = create(:invoice_item, quantity: 1, unit_price: 10000, item_id: @item_1.id, invoice_id: @invoice.id, status: 1)
+    @invoice_item_2 = create(:invoice_item, quantity: 5 , unit_price: 5000, item_id: @item_2.id, invoice_id: @invoice.id, status: 1)
+    @invoice_item_3 = create(:invoice_item, quantity: 15 , unit_price: 4000, item_id: @item_3.id, invoice_id: @invoice.id, status: 1)
+
+    @bulk_discount = BulkDiscount.create!(name: 'BD 2', percentage: 15, threshold: 10, merchant: @merchant)
+
+    visit admin_invoice_path(@invoice)
   end
 
   it 'shows information regarding the invoice' do
-    visit admin_invoice_path(@invoice_1)
-
-    expect(page).to have_content(@invoice_1.id)
-    expect(page).to have_content(@invoice_1.created_at_display)
-    expect(page).to have_content(@invoice_1.customer.first_name)
-    expect(page).to have_content(@invoice_1.customer.last_name)
+    expect(page).to have_content(@invoice.id)
+    expect(page).to have_content(@invoice.created_at_display)
+    expect(page).to have_content(@invoice.customer.first_name)
+    expect(page).to have_content(@invoice.customer.last_name)
   end
 
   it 'shows invoice items and item information' do
-    visit admin_invoice_path(@invoice_1)
-
     expect(page).to have_content(@item_1.name)
     expect(page).to have_content(@invoice_item_1.quantity)
     expect(page).to have_content(@invoice_item_1.unit_price / 100)
     expect(page).to have_content(@invoice_item_1.status)
   end
 
-  it 'shows total revenue to be earned through invoice' do
-    visit admin_invoice_path(@invoice_1)
-    expect(page).to have_content('Total Projected Revenue: $111.11')
-  end
-
   it 'displays select field with current invoice status selected' do
-    visit admin_invoice_path(@invoice_1)
     expect(page).to have_content('Status:')
-    expect(page).to have_select(selected: "#{@invoice_1.status.titleize}")
+    expect(page).to have_select(selected: "#{@invoice.status.titleize}")
   end
 
   it 'can update status using select field' do
-    visit admin_invoice_path(@invoice_1)
-    expect(@invoice_1.status).to eq('in progress')
+    expect(@invoice.status).to eq('in progress')
     select('Completed')
     click_button 'Update Invoice'
-    @invoice_1.reload
-    expect(@invoice_1.status).to eq('completed')
+    @invoice.reload
+    expect(@invoice.status).to eq('completed')
+  end
+
+  it 'shows total revenue to be earned through invoice' do
+    expect(page).to have_content("Total Projected Revenue: $#{@invoice.total_revenue}")
+  end
+
+  it 'displays total discoutned revenue to be earned through invoice' do
+    expect(page).to have_content("Total Discounted Revenue: $#{@invoice.total_discounted_revenue}")
   end
 end
